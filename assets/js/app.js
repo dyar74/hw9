9db1c505a7fb4601fedc27df884f27a28f9064db
Yaroslav Duplyak <mudrui@gmail.com>
2018-02-26 17:24:33 +0200

    b1

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+var $ = require('jquery');
+// JS is equivalent to the normal "bootstrap" package
+// no need to set this to a variable, just require it
+require('bootstrap-sass');
+require('bootstrap-star-rating');
+// require 2 CSS files needed
+require('bootstrap-star-rating/css/star-rating.css');
+require('bootstrap-star-rating/themes/krajee-svg/theme.css');
